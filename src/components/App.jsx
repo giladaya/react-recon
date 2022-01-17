27116de4aad7761fa4adfb77b2cafe301bb4a69c
Gilad <giladaya@gmail.com>
2022-01-17 17:49:45 +0200

    Minor fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,43 +5,54 @@ import "./App.css";
 const INITIAL_STATE = [
   {
     text: "Buy milk",
-    id: 1
+    id: 1,
   },
   {
     text: "Clean room",
-    id: 2
+    id: 2,
   },
   {
     text: "Prepare presentation",
-    id: 3
+    id: 3,
   },
   {
     text: "Practice piano",
-    id: 4
-  }
+    id: 4,
+  },
 ];
 
 export default function App() {
+  console.log("========================");
   console.log("Render App");
 
-  // Application state
+  //-------
+  // State
+  //-------
   const [todos, setTodos] = React.useState(INITIAL_STATE);
   const [inputValue, setInputValue] = React.useState("");
 
+  //------------------------
   // State update functions
-  const handleChange = React.useCallback(ev => {
+  //------------------------
+  const handleChange = React.useCallback((ev) => {
     setInputValue(ev.target.value);
   }, []);
 
   const handleAdd = React.useCallback(() => {
+    if (inputValue.length === 0) {
+      return;
+    }
     setTodos([...todos, { text: inputValue, id: Date.now() }]);
     setInputValue("");
   }, [inputValue, todos]);
 
-  const handleItemCheck = itemId => {
-    setTodos(todos.filter(item => item.id !== itemId));
+  const handleItemCheck = (itemId) => {
+    setTodos(todos.filter((item) => item.id !== itemId));
   };
 
+  //--------
+  // Render
+  //--------
   return (
     <div className="App">
       <h1>What TODO?</h1>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ListItem from "./ListItem";
 
 function List({ todos, onCheck }) {
-  console.log('Render List');
+  console.log("Render List");
 
   // const Component = todos.length % 2 === 0 ? 'ol' : 'ul';
-  const Component = 'ul';
+  const Component = "ul";
 
   return (
     <Component className="list">
